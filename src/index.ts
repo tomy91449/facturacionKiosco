ba99052914fdbdf5de84a965af4ede300b858944
Tomás Emanuel Julio <91449@sistemas.frc.utn.edu.ar>
2026-05-16 10:49:37 +0000

    Con front ahora

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { apiRouter } from "./api/router";
 
 dotenv.config();
@@ -8,6 +9,11 @@ const app  = express();
 const PORT = process.env.PORT ?? 4000;
 
 app.use(express.json());
+
+// Servir el frontend estático desde la carpeta /public
+app.use(express.static(path.join(process.cwd(), "public")));
+
+// Rutas de la API
 app.use("/", apiRouter);
 
 app.listen(PORT, () => {
